45cb99d017b506d608f276ee9aee47c8ba58d5b9
SilverFlin <toledorusso@outlook.com>
2023-02-15 16:43:50 -0700

    fix: estadoCuenta en CuentaBancaria

--- codigo/BancoMaven/src/main/java/dominio/CuentaBancaria.java
+++ codigo/BancoMaven/src/main/java/dominio/CuentaBancaria.java
@@ -12,44 +12,74 @@ import java.util.Objects;
  * @author Toled
  */
 public class CuentaBancaria {
+
     private Integer id;
     private String noCuenta;
     private String fechaApertura;
     private Double saldoMXN;
     private Cliente clientePropietario;
-    
+    private EstadoCuenta estadoCuenta;
+
+    private enum EstadoCuenta {
+        ACTIVO, INACTIVO
+    };
+
     public CuentaBancaria() {
         generarNoCuenta();
         this.saldoMXN = 0.0;
+        this.estadoCuenta = EstadoCuenta.ACTIVO;
     }
-    
-    
-   private void generarNoCuenta(){
-       /*Numero aleatorio de 16 digitos*/
-       long rawNumCuenta = (long) (Math.random() * 9000_0000_0000_0000L + 1000_0000_0000_0000L );
-       
-       /*Separa el numero*/
-       String[] arrNumCuenta = Long.toString(rawNumCuenta).split("(?<=\\G....)");
-       String numCuenta = "";
-       for (int i = 0; i < 4; i++) {
-           if(i!=3){
-               numCuenta += arrNumCuenta[i] + " ";
-           }else numCuenta += arrNumCuenta[i];
-       }
-       
-       this.noCuenta = numCuenta;
-   }
-   
-   /**
-    * Agrega saldo a la cuenta bancaria
-    * @param cantidad 
-    */
-   public void agregarSaldo(double cantidad){
-       
-       if(cantidad <= 0)return;
-       
-       this.setSaldoMXN(this.getSaldoMXN() + cantidad);
-   }
+
+    /**
+     * Genera un String de 16 numeros con espaciado cada 4 y lo guarda en
+     * noCuenta
+     */
+    private void generarNoCuenta() {
+        /*Numero aleatorio de 16 digitos*/
+        long rawNumCuenta = (long) (Math.random() * 9000_0000_0000_0000L
+                + 1000_0000_0000_0000L);
+
+        /*Separa el numero*/
+        String[] arrNumCuenta = Long.toString(rawNumCuenta).split("(?<=\\G....)");
+        String numCuenta = "";
+        for (int i = 0; i < 4; i++) {
+            if (i != 3) {
+                numCuenta += arrNumCuenta[i] + " ";
+            } else {
+                numCuenta += arrNumCuenta[i];
+            }
+        }
+
+        this.noCuenta = numCuenta;
+    }
+
+    /**
+     * Agrega saldo a la cuenta bancaria
+     *
+     * @param cantidad
+     */
+    public void agregarSaldo(double cantidad) {
+
+        if (cantidad <= 0) {
+            return;
+        }
+
+        this.setSaldoMXN(this.getSaldoMXN() + cantidad);
+    }
+
+    /**
+     * Establece el estado de cuenta a Activo
+     */
+    public void activarCuenta() {
+        this.estadoCuenta = EstadoCuenta.ACTIVO;
+    }
+
+    /**
+     * Establece el estado de cuenta a Inactivo
+     */
+    public void desactivarCuenta() {
+        this.estadoCuenta = EstadoCuenta.INACTIVO;
+    }
 
     public Integer getId() {
         return id;
@@ -71,6 +101,10 @@ public class CuentaBancaria {
         return clientePropietario;
     }
 
+    public EstadoCuenta getEstadoCuenta() {
+        return estadoCuenta;
+    }
+
     public void setSaldoMXN(Double saldoMXN) {
         this.saldoMXN = saldoMXN;
     }
@@ -78,8 +112,11 @@ public class CuentaBancaria {
     public void setClientePropietario(Cliente clientePropietario) {
         this.clientePropietario = clientePropietario;
     }
-    
-    
+
+    public void setEstadoCuenta(EstadoCuenta estadoCuenta) {
+        this.estadoCuenta = estadoCuenta;
+    }
+
     @Override
     public String toString() {
         return "CuentaBancaria{" + "id=" + id + ", noCuenta=" + noCuenta + ", fechaApertura=" + fechaApertura + ", saldoMXN=" + saldoMXN + '}';
@@ -113,9 +150,5 @@ public class CuentaBancaria {
         }
         return true;
     }
-   
-    
-    
-   
-    
+
 }
