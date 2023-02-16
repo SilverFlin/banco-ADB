c50851ad38b46cedd19065e16b6678971c003a0a
Marki <124947044+MarkiHub@users.noreply.github.com>
2023-02-15 21:48:58 -0700

    feat: interfaz para domicilios

---
+++ codigo/BancoMaven/src/main/java/interfaces/IDomiciliosDAO.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package interfaces;
+
+import dominio.Domicilio;
+import excepciones.PersistenciaException;
+import java.util.List;
+import utils.ConfiguracionPaginado;
+
+/**
+ *
+ * @author Elkur
+ */
+public interface IDomiciliosDAO {
+
+    Domicilio consultar(int id);
+
+    Domicilio insertar(Domicilio domicilio) throws PersistenciaException;
+
+    Domicilio editar(Domicilio domicilio) throws PersistenciaException;
+
+}
