68d9f06e1718a97539d03e1fb1aa1fc812adb3d0
SilverFlin <toledorusso@outlook.com>
2023-02-19 15:38:35 -0700

    test: entorno pruebas

--- codigo/BancoMaven/src/main/java/main/Main.java
+++ codigo/BancoMaven/src/main/java/main/Main.java
@@ -13,10 +13,12 @@ import dominio.EstadoRetiroSinCuenta;
 import dominio.RetiroSinCuenta;
 import dominio.Transferencia;
 import excepciones.PersistenciaException;
+import implementaciones.ClientesDAO;
 import implementaciones.ConexionBD;
 import implementaciones.CuentasBancariasDAO;
 import implementaciones.RetirosSinCuentaDAO;
 import implementaciones.TransferenciasDAO;
+import interfaces.IClientesDAO;
 import interfaces.IConexionBD;
 import interfaces.ICuentasBancariasDAO;
 import interfaces.IRetirosSinCuentaDAO;
@@ -26,7 +28,8 @@ import java.sql.SQLException;
 import java.sql.Timestamp;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import presentacion.ClienteForm;
+import org.mindrot.jbcrypt.BCrypt;
+import presentacion.IniciarSesionForm;
 import presentacion.CuentasForm;
 import presentacion.MenuPrincipalForm;
 import presentacion.RegistroClienteForm;
@@ -46,55 +49,19 @@ public class Main {
         IConexionBD conexion = new ConexionBD("jdbc:mysql://localhost:3306/banco_transacciones", "root", System.getenv("passSQL"));
 
         /*Prueba Cuenta Bancaria*/
-        ICuentasBancariasDAO cuentasBancariasDAO = new CuentasBancariasDAO(conexion);
-//        CuentaBancaria cuentaBancaria;
-        Cliente cliente = new Cliente(101);
-//        MenuPrincipalForm menuPrincipal = new MenuPrincipalForm(cuentasBancariasDAO,cliente);
-//        menuPrincipal.setVisible(true);
+        IClientesDAO clientesDAO = new ClientesDAO(conexion);
+        Cliente cliente = new Cliente("Luis", "Toledo", "Russo", "2022-12-06", 1);
+        cliente.setCorreo("admin");
+        cliente.setContrasenia(BCrypt.hashpw("admin", BCrypt.gensalt()));
+        try {
+            clientesDAO.insertar(cliente);
+        } catch (PersistenciaException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
 
-        /*Prueba transferencia*/
-//        ITransferenciasDAO transferenciasDAO = new TransferenciasDAO(conexion);
-//        try {
-//            transferenciasDAO.insertar(new Transferencia("2021-04-24 21:26:41", 15.3, 2, 5));
-//            transferenciasDAO.insertar(new Transferencia("2021-04-24 21:26:41", 15.3, 2, 6));
-//            System.out.println(transferenciasDAO.consultarOrigen(new ConfiguracionPaginado(), 2));
-//        } catch (PersistenciaException ex) {
-//            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
-//        }
-//
-//        ClienteForm cuentasForm = new ClienteForm(conexion);
-//        cuentasForm.setVisible(true);
-
-        /*Prueba Retiros*/
-        IRetirosSinCuentaDAO retirosSinCuentaDAO = new RetirosSinCuentaDAO(conexion);
-//        try {
-//            CuentaBancaria cuentaBancaria = cuentasBancariasDAO.consultar(1);
-//            System.out.println(cuentaBancaria);
-//            RetiroSinCuenta retiroSinCuenta = new RetiroSinCuenta(
-//                    "2021-04-24 21:26:41",
-//                    "2021-05-24 21:26:41", 
-//                    EstadoRetiroSinCuenta.PENDIENTE,
-//                    15.3,
-//                    "123123",
-//                    "33333", 
-//                    cuentaBancaria.getId());
-//            
-//            
-//            retiroSinCuenta = retirosSinCuentaDAO.insertar(retiroSinCuenta, cuentaBancaria);
-//            System.out.println(retirosSinCuentaDAO.consultar("33333"));
-//            System.out.println(retirosSinCuentaDAO.consultar(retiroSinCuenta.getId()));
-//            retiroSinCuenta.setEstado(EstadoRetiroSinCuenta.EXPIRADO);
-//            retirosSinCuentaDAO.actualizar(retiroSinCuenta);
-//            System.out.println(retirosSinCuentaDAO.consultar(retiroSinCuenta.getId()));
-//            retirosSinCuentaDAO.eliminar(retiroSinCuenta.getId());
-//            System.out.println(retirosSinCuentaDAO.consultar(retiroSinCuenta.getId()));
-//        } catch (PersistenciaException e) {
-//            System.out.println(e.getMessage());
-//        }
-            ClienteForm clienteForm = new ClienteForm(conexion);
-            clienteForm.setVisible(true);
-
-//            6068 6841 2454 6781 > 69390936
+        IniciarSesionForm clienteForm = new IniciarSesionForm(conexion);
+        clienteForm.setVisible(true);
+        
     }
 
 }
