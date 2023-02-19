808806867d85533b0756357eb281c7b76d6edc6c
Marki <124947044+MarkiHub@users.noreply.github.com>
2023-02-18 20:59:46 -0700

    feat: interfaz operacionesDAO

---
+++ codigo/BancoMaven/src/main/java/interfaces/IOperacionesDAO.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package interfaces;
+
+import dominio.Operacion;
+import excepciones.PersistenciaException;
+import java.util.List;
+import utils.ConfiguracionPaginado;
+
+/**
+ *
+ * @author Elkur
+ */
+public interface IOperacionesDAO {
+
+    public Operacion consultar(int id);
+
+    public Operacion insertar(Operacion operacion) throws PersistenciaException;
+
+    List<Operacion> consultar(ConfiguracionPaginado configPaginado) throws PersistenciaException;
+
+}
