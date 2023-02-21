8fdb88cab30a8e19521cb7e4d20b2d7ca97f100b
Marki <124947044+MarkiHub@users.noreply.github.com>
2023-02-21 03:26:06 -0700

    fix: documentacion

--- codigo/BancoMaven/src/main/java/implementaciones/ClientesDAO.java
+++ codigo/BancoMaven/src/main/java/implementaciones/ClientesDAO.java
@@ -21,24 +21,48 @@ import utils.ConfiguracionPaginado;
  */
 public class ClientesDAO implements IClientesDAO {
 
+    /**
+     * Logger de excepciones
+     */
     private static final Logger LOG = Logger.getLogger(ClientesDAO.class.getName());
+    /**
+     * Generador de conexiones
+     */
     private final IConexionBD GENERADOR_CONEXIONES;
 
+    /**
+     * Constructor que recibe el generador de conexiones
+     *
+     * @param GENERADOR_CONEXIONES Generador de conexiones
+     */
     public ClientesDAO(IConexionBD GENERADOR_CONEXIONES) {
         this.GENERADOR_CONEXIONES = GENERADOR_CONEXIONES;
     }
 
+    /**
+     * Consulta en la base de datos el cliente que tenga el correo mandado en el
+     * parametro
+     *
+     * @param correo Correo a comparar
+     * @return Cliente que tenga el correo
+     */
     @Override
     public Cliente consultar(String correo) {
+        /* Consultas */
         String SQLQuery = "SELECT id, nombres, apellidoPaterno,apellidoMaterno, fechaNacimiento,idDomicilio,password FROM CLIENTES WHERE correo=?;";
 
         try (
                 Connection conexion = this.GENERADOR_CONEXIONES.crearConexion(); PreparedStatement comando = conexion.prepareStatement(SQLQuery);) {
-
+            /*
+            Establecemos el correo del parametro en la consulta
+             */
             comando.setString(1, correo);
             ResultSet resultado = comando.executeQuery();
 
             Cliente cliente = null;
+            /*
+            Extraemos el cliente de la consulta
+             */
             if (resultado.next()) {
                 int id = resultado.getInt("id");
                 String nombre = resultado.getString("nombres");
@@ -50,22 +74,37 @@ public class ClientesDAO implements IClientesDAO {
                 cliente.setContrasenia(resultado.getString("password"));
             }
 
+            /*
+            Regresamos el cliente obtenido
+             */
             return cliente;
 
         } catch (SQLException ex) {
             LOG.log(Level.SEVERE, ex.getMessage());
+            /*
+            En caso de alguna excepcion se regresa null de todas maneras
+             */
             return null;
         }
     }
 
+    /**
+     * Inserta en la base de datos a un cliente dado en el parametro
+     *
+     * @param cliente Cliente a insertar
+     * @return Cliente insertado
+     * @throws PersistenciaException Si no se puede acceder a la base
+     */
     @Override
     public Cliente insertar(Cliente cliente) throws PersistenciaException {
+        /* Consultas */
         String SQLQuery = "INSERT INTO clientes(nombres,apellidoPaterno, apellidoMaterno, fechaNacimiento, idDomicilio, correo, password) VALUES (?,?,?,?,?,?,?)";
 
-        try (Connection conexion = GENERADOR_CONEXIONES.crearConexion(); 
-                PreparedStatement comando = conexion.prepareStatement(SQLQuery, Statement.RETURN_GENERATED_KEYS);
-                ) {
+        try (Connection conexion = GENERADOR_CONEXIONES.crearConexion(); PreparedStatement comando = conexion.prepareStatement(SQLQuery, Statement.RETURN_GENERATED_KEYS);) {
 
+            /*
+            Establecemos los atributos del cliente en la insercion
+             */
             comando.setString(1, cliente.getNombres());
             comando.setString(2, cliente.getApellidoP());
             comando.setString(3, cliente.getApellidoM());
@@ -77,11 +116,20 @@ public class ClientesDAO implements IClientesDAO {
 
             ResultSet llavesGeneradas = comando.getGeneratedKeys();
 
+            /*
+            Extraemos la llave primaria que se genero en la insercion
+             */
             if (llavesGeneradas.next()) {
                 Integer llavePrimaria = llavesGeneradas.getInt(1);
                 cliente.setId(llavePrimaria);
+                /*
+                Regresamos al cliente con su id generada
+                 */
                 return cliente;
             }
+            /**
+             * En caso de que no se genere una llave, se lanza una excepcion
+             */
             throw new PersistenciaException("Cliente registrado pero ID no generado");
 
         } catch (SQLException e) {
@@ -91,17 +139,31 @@ public class ClientesDAO implements IClientesDAO {
         }
     }
 
-
+    /**
+     * Actualiza al cliente en la base de datos que coincida con el del
+     * parametro
+     *
+     * @param cliente Cliente a actualizar
+     * @return Cliente actualizado
+     * @throws PersistenciaException Si no se tiene acceso a la base
+     */
     @Override
     public Cliente actualizar(Cliente cliente) throws PersistenciaException {
+        /* Consultas */
         String SQLQuery = "UPDATE clientes SET password = ? WHERE id = ?";
 
         try (Connection conexion = GENERADOR_CONEXIONES.crearConexion(); PreparedStatement comando = conexion.prepareStatement(SQLQuery);) {
 
+            /*
+            Establecemos los parametros del update
+             */
             comando.setString(1, cliente.getContrasenia());
             comando.setInt(2, cliente.getId());
 
             int numeroClientesEditados = comando.executeUpdate();
+            /*
+            Si lo editamos regresamos el cliente, si no mandamos null
+             */
             return numeroClientesEditados > 0 ? cliente : null;
 
         } catch (SQLException e) {
@@ -111,33 +173,48 @@ public class ClientesDAO implements IClientesDAO {
         }
     }
 
+    /**
+     * Consulta a los clientes con una configuracion de paginado
+     *
+     * @param configPaginado Configuracion del paginado
+     * @return Lista de clientes
+     * @throws PersistenciaException Si no se tiene acceso a la base
+     */
     @Override
     public List<Cliente> consultar(ConfiguracionPaginado configPaginado) throws PersistenciaException {
+        /* Consultas */
         String SQLQuery = "SELECT id, nombre, apellidoPaterno, apellidoMaterno, fechaNacimiento, idDomicilio "
                 + "FROM clientes LIMIT ? OFFSET ?";
         List<Cliente> listaClientes = new LinkedList<>();
         try (
-                Connection conexion = this.GENERADOR_CONEXIONES.crearConexion(); 
-                PreparedStatement comando = conexion.prepareStatement(SQLQuery);
-                ) {
+                Connection conexion = this.GENERADOR_CONEXIONES.crearConexion(); PreparedStatement comando = conexion.prepareStatement(SQLQuery);) {
 
+            /*
+            Establecemos los atributos del paginado a la consulta
+             */
             comando.setInt(1, configPaginado.getLimite());
             comando.setInt(2, configPaginado.getOffset());
             ResultSet resultado = comando.executeQuery();
 
+            /*
+            Guardamos los resultados en una lista
+             */
             while (resultado.next()) {
-                
+
                 Integer id = resultado.getInt("id");
                 String nombre = resultado.getString("nombres");
                 String apellido_paterno = resultado.getString("apellidoPaterno");
                 String apellido_materno = resultado.getString("apellidoMaterno");
                 String fechaNacimiento = resultado.getString("fechaNacimiento");
                 Integer idDireccion = resultado.getInt("idDomicilio");
-                
-                Cliente cliente = new Cliente(id, nombre, apellido_paterno, apellido_materno,fechaNacimiento, idDireccion);
+
+                Cliente cliente = new Cliente(id, nombre, apellido_paterno, apellido_materno, fechaNacimiento, idDireccion);
                 listaClientes.add(cliente);
             }
 
+            /*
+            Regresamos la lista de clientes
+             */
             return listaClientes;
         } catch (SQLException ex) {
             LOG.log(Level.SEVERE, ex.getMessage());
--- codigo/BancoMaven/src/main/java/implementaciones/ConexionBD.java
+++ codigo/BancoMaven/src/main/java/implementaciones/ConexionBD.java
@@ -10,17 +10,37 @@ import java.sql.SQLException;
  * @author Elkur
  */
 public class ConexionBD implements IConexionBD {
-
+    
+    /**
+     * Cadena de  texto que contiene la informacion de conexion a la base
+     */
     private final String CADENA_CONEXION;
+    /**
+     * Usuario de acceso a la base
+     */
     private final String USUARIO;
+    /**
+     * Contrasenia de acceso a la base
+     */
     private final String PASSWORD;
-
+    
+    /**
+     * Constructor que recibe las credenciales de acceso a la base
+     * @param CADENA_CONEXION Cadena de  texto que contiene la informacion de conexion a la base
+     * @param USUARIO Usuario de acceso a la base
+     * @param PASSWORD Contrasenia de acceso a la base
+     */
     public ConexionBD(String CADENA_CONEXION, String USUARIO, String PASSWORD) {
         this.CADENA_CONEXION = CADENA_CONEXION;
         this.USUARIO = USUARIO;
         this.PASSWORD = PASSWORD;
     }
-
+    
+    /**
+     * Establece la conexion con la base con las credenciales que almacena
+     * @return Objeto de gestion de conexion
+     * @throws SQLException Si ocurre una excepcion al conectarse
+     */
     @Override
     public Connection crearConexion() throws SQLException {
         Connection conexion = DriverManager.getConnection(CADENA_CONEXION, USUARIO, PASSWORD);
