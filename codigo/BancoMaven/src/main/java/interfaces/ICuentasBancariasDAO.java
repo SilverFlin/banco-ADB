fb36aa61c19c01ee158f980e0722bf98a70c0f32
Marki <124947044+MarkiHub@users.noreply.github.com>
2023-02-21 03:29:18 -0700

    fix: documentacion

--- codigo/BancoMaven/src/main/java/interfaces/ICuentasBancariasDAO.java
+++ codigo/BancoMaven/src/main/java/interfaces/ICuentasBancariasDAO.java
@@ -12,16 +12,66 @@ import utils.ConfiguracionPaginado;
  */
 public interface ICuentasBancariasDAO {
 
+    /**
+     * Regresa la cuenta bancaria en base al id proporcionado
+     *
+     * @param id Identificador de la cuenta bancaria
+     * @return Cuenta bancaria encontrada
+     * @throws PersistenciaException Si ocurrio una exception al conectarse con
+     * la base
+     */
     CuentaBancaria consultar(int id) throws PersistenciaException;
 
+    /**
+     * Consulta la base de datos de cuentas bancarias y trae la que tenga el
+     * numero de cuenta especificado
+     *
+     * @param noCuenta Numero de cuenta a buscar
+     * @return Cuenta bancaria
+     * @throws PersistenciaException Si ocurre una excepcion al conectarse a la
+     * base de datos
+     */
     CuentaBancaria consultar(String noCuenta) throws PersistenciaException;
 
+    /**
+     * Consulta las cuentas bancarias asociadas al identificador del cliente
+     * dado
+     *
+     * @param configPaginado Configuracion del paginado
+     * @param idCliente identificador del cliente que se le buscan
+     * @return Lista de cuentas asociadas
+     * @throws PersistenciaException Si ocurre una excepcion al conectarse a la
+     * base
+     */
     List<CuentaBancaria> consultar(ConfiguracionPaginado configPaginado, int idCliente) throws PersistenciaException;
 
+    /**
+     * Inserta una cuenta bancaria, relacionandose con el id de un cliente.
+     *
+     * @param cuentaBancaria Cuenta bancaria a insertar
+     * @param cliente Cliente a emplear
+     * @return la cuenta bancaria, pero con el id asignado por la base de datos.
+     * @throws PersistenciaException
+     */
     CuentaBancaria insertar(CuentaBancaria cuentaBancaria, Cliente cliente) throws PersistenciaException;
 
+    /**
+     * Actualiza la informacion de la cuenta bancaria en la base de datos con la
+     * informacion de la cuenta dada
+     *
+     * @param cuentaBancaria Cuenta a actualizar
+     * @return Cuenta actualizada
+     * @throws PersistenciaException Si ocurre una excepcion al conectarse a la
+     * base
+     */
     CuentaBancaria actualizar(CuentaBancaria cuentaBancaria) throws PersistenciaException;
 
+    /**
+     * Elimina la cuenta bancaria de la base de datos, usando su id
+     *
+     * @param id identificador dela cuenta a eliminar
+     * @return la cuenta bancaria eliminada o null si no existia
+     */
     CuentaBancaria eliminar(Integer id);
 
 }
--- codigo/BancoMaven/src/main/java/interfaces/IDomiciliosDAO.java
+++ codigo/BancoMaven/src/main/java/interfaces/IDomiciliosDAO.java
@@ -9,10 +9,33 @@ import excepciones.PersistenciaException;
  */
 public interface IDomiciliosDAO {
 
+    /**
+     * Consulta la base de datos y trae el domicilio que coincida con el id dada
+     *
+     * @param id identificador a buscar
+     * @return Domicilio encontrado
+     */
     Domicilio consultar(int id);
 
+    /**
+     * Inserta en la base de datos el domicilio dado
+     *
+     * @param domicilio Domicilio a insertar
+     * @return Domicilio insertado
+     * @throws PersistenciaException Si ocurre una excepcion al conectarse a la
+     * base
+     */
     Domicilio insertar(Domicilio domicilio) throws PersistenciaException;
 
+    /**
+     * Actualiza el domicilio en la base que coincida con el dado en el
+     * parametro
+     *
+     * @param domicilio Domicilio a actualizar
+     * @return Domicilio actualizado
+     * @throws PersistenciaException Si ocurre una excepcion al conectarse a la
+     * base
+     */
     Domicilio actualizar(Domicilio domicilio) throws PersistenciaException;
 
 }
