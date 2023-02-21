d5a80049a5a4647be26e35588738f60ad2384c31
Marki <124947044+MarkiHub@users.noreply.github.com>
2023-02-21 03:29:32 -0700

    fix: documentacion

--- codigo/BancoMaven/src/main/java/interfaces/IOperacionesDAO.java
+++ codigo/BancoMaven/src/main/java/interfaces/IOperacionesDAO.java
@@ -11,12 +11,46 @@ import utils.ConfiguracionPaginado;
  */
 public interface IOperacionesDAO {
 
+    /**
+     * Consulta la base de datos y trae la operacion que coincida con la id dada
+     *
+     * @param id identificador a encontrar
+     * @return Operacion encontrada
+     */
     public Operacion consultar(int id);
 
+    /**
+     * Consulta la lista de operaciones de una cuenta bancaria mediante su ID
+     *
+     * @param configPaginado Configuracion del paginado
+     * @param idCuentaBancaria Identificador de la cuenta para buscar
+     * operaciones
+     * @return Lista de operaciones de la cuenta
+     * @throws PersistenciaException Si ocurre una excepcion al conectarse a la
+     * base
+     */
     List<Operacion> consultar(ConfiguracionPaginado configPaginado, int idCuentaBancaria) throws PersistenciaException;
 
+    /**
+     * Consulta en la base la lista de operaciones asociadas al id del cliente
+     * dado
+     *
+     * @param configPaginado Configuracion de paginado
+     * @param idCliente Identificador del cliente para buscar
+     * @return Lista de operaciones asociadas a la cuenta
+     * @throws PersistenciaException Si ocurre una excepcion al conectarse a la
+     * base
+     */
     List<Operacion> consultar(ConfiguracionPaginado configPaginado, String idCliente) throws PersistenciaException;
 
+    /**
+     * Inserta en la base de datos la operacion dada
+     *
+     * @param operacion Operacion a insertar
+     * @return Operacion insertada
+     * @throws PersistenciaException Si ocurre una excepcion al conectarse a la
+     * base
+     */
     public Operacion insertar(Operacion operacion) throws PersistenciaException;
 
 }
--- codigo/BancoMaven/src/main/java/interfaces/ITransferenciasDAO.java
+++ codigo/BancoMaven/src/main/java/interfaces/ITransferenciasDAO.java
@@ -11,14 +11,56 @@ import utils.ConfiguracionPaginado;
  */
 public interface ITransferenciasDAO {
 
+    /**
+     * Consulta en la base la transferencia mediante la cuenta origen
+     *
+     * @param idCuentaOrigen Identificador de la cuenta origen
+     * @return Transferencia encontrada
+     * @throws PersistenciaException Si ocurre una excepcion al consultar la
+     * base
+     */
     Transferencia consultarOrigen(int idCuentaOrigen) throws PersistenciaException;
 
+    /**
+     * Consulta la base de trasferencias asociadas a una cuenta origen
+     *
+     * @param configPaginado Configuracion de paginado
+     * @param idCuentaOrigen Identificador de la cuenta origen
+     * @return Lista de transferencias
+     * @throws PersistenciaException Si ocurre una excepcion al consultar la
+     * base
+     */
     List<Transferencia> consultarOrigen(ConfiguracionPaginado configPaginado, int idCuentaOrigen) throws PersistenciaException;
 
+    /**
+     * Consulta en la base la transferencia mediante la cuenta destino
+     *
+     * @param idCuentaDestino Identificador de la cuenta destino
+     * @return Transferencia encontrada
+     * @throws PersistenciaException Si ocurre una excepcion al consultar la
+     * base
+     */
     Transferencia consultarDestino(int idCuentaDestino) throws PersistenciaException;
 
+    /**
+     * Consulta la base de trasferencias asociadas a una cuenta destino
+     *
+     * @param configPaginado Configuracion de paginado
+     * @param idCuentaDestino Identificador de la cuenta destino
+     * @return Lista de transferencias
+     * @throws PersistenciaException Si ocurre una excepcion al consultar la
+     * base
+     */
     List<Transferencia> consultarDestino(ConfiguracionPaginado configPaginado, int idCuentaDestino) throws PersistenciaException;
 
+    /**
+     * Inserta en la base una transferencia dada
+     *
+     * @param transferencia Transferencia a insertar
+     * @return Transferencia insertada
+     * @throws PersistenciaException Si ocurre una excepcion al consultar la
+     * base
+     */
     Transferencia insertar(Transferencia transferencia) throws PersistenciaException;
 
 }
