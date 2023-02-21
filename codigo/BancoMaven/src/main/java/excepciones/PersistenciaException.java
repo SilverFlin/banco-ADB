8d1d48a3189b5bc7890643480a68021b5b87800f
Marki <124947044+MarkiHub@users.noreply.github.com>
2023-02-21 03:25:20 -0700

    fix: documentacion

--- codigo/BancoMaven/src/main/java/excepciones/PersistenciaException.java
+++ codigo/BancoMaven/src/main/java/excepciones/PersistenciaException.java
@@ -6,21 +6,48 @@ package excepciones;
  */
 public class PersistenciaException extends Exception {
 
+    /**
+     * Constructor vacio
+     */
     public PersistenciaException() {
     }
 
+    /**
+     * Constructor que recibe el mensaje de la excepcion
+     *
+     * @param string mensaje de la excepcion
+     */
     public PersistenciaException(String string) {
         super(string);
     }
 
+    /**
+     * Constructor
+     *
+     * @param string Mensaje
+     * @param thrwbl No se
+     */
     public PersistenciaException(String string, Throwable thrwbl) {
         super(string, thrwbl);
     }
 
+    /**
+     * Constructor
+     *
+     * @param thrwbl mensaje
+     */
     public PersistenciaException(Throwable thrwbl) {
         super(thrwbl);
     }
 
+    /**
+     * Constructor
+     *
+     * @param string Mensaje
+     * @param thrwbl Mensaje
+     * @param bln booleano
+     * @param bln1 booleano
+     */
     public PersistenciaException(String string, Throwable thrwbl, boolean bln, boolean bln1) {
         super(string, thrwbl, bln, bln1);
     }
